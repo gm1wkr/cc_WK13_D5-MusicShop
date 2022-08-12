feab78dfb9655e6f1ce36897d3b5b7f91f224a35
Mike GM1WKR <gm1wkr@gmail.com>
2022-08-12 17:14:29 +0100

    added

---
+++ src/main/java/MusicShop.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class MusicShop {
+
+    ArrayList<ISell> stock;
+
+
+}
---
+++ src/test/java/MusicShopTest.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+
+public class MusicShopTest {
+
+    MusicShop musicShop;
+
+    @Before
+    public void before(){
+        musicShop = new MusicShop();     
+    }
+}
